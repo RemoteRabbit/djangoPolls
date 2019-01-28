ff8d121410a09b5b427ffa12bddd9efc411e1c19
Tristan Jahnke <trjahnke@gmail.com>
2019-01-28 14:29:22 -0600

    revamped admin and added todo

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # djangoPolls
 [![Build Status](https://travis-ci.org/trjahnke/djangoPolls.svg?branch=master)](https://travis-ci.org/trjahnke/djangoPolls)\
 Django Project polls web application
+
+# ToDo - not in any order
+-Landing page
+    - Make one
+    - Set it up to override django sign on and sign up 
+- Setup user login and creation 
+    - at somepoint set this up to do third party login
+- fancy up the admin console
+- page redesigns
+- add additional testing for better coverage
--- polls/admin.py
+++ polls/admin.py
@@ -1,4 +1,21 @@
 from django.contrib import admin
-from .models import Question
+from .models import Choice, Question
 
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 0
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date Information", {"fields": ["pub_date"]}),
+    ]
+    inlines = [ChoiceInline]
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
+    list_per_page = 10
+
+    list_display = ("question_text", "pub_date", "was_published_recently")
+
+
+admin.site.register(Question, QuestionAdmin)
